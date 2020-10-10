1bac4818dec0722f8b0751082c3ea21f6f255cbc
ALEKSEI_RYBALKIN <ALEKSEI.RYBALKIN@SIMBIRSOFT.COM>
2020-10-10 22:42:18 +0400

    Tests for Create and Delete folder have been added.

--- src/test/java/api/requests/DiskManagementRequest.java
+++ src/test/java/api/requests/DiskManagementRequest.java
@@ -1,33 +1,56 @@
 package api.requests;
 
 import io.qameta.allure.Step;
+import io.restassured.response.Response;
+
 import static io.restassured.module.jsv.JsonSchemaValidator.*;
 import static org.hamcrest.Matchers.equalTo;
 
 public class DiskManagementRequest extends BaseRequest{
     public String path = "/v1/disk/resources";
 
-    @Step("GET Запрос на получение метаинформации о диске пользователя")
-    public void getDiskMetaInfo(){
-        givenWithAuth()
+    @Step("GET Запрос: Получение метаинформации о диске пользователя")
+    public Response getDiskMetaInfo(){
+        return givenWithAuth()
                 .spec(getRequestBuilder(host + "/v1/disk").build())
                 .when()
                 .get()
                 .then()
-                .assertThat()
-                .body(matchesJsonSchemaInClasspath("getMetaInfo.json"));
+                .extract()
+                .response();
     }
 
-    @Step("GET Запрос на получение метаинформации о диске пользователя")
-    public void getFileOrFolderMetaInfo(){
-        givenWithAuth()
-                .spec(getRequestBuilder(host + path + "?path=/").build())
+    @Step("GET Запрос: Получение метаинформации о диске пользователя")
+    public Response getFileOrFolderMetaInfo(String pathToFileOrFolder){
+        return givenWithAuth()
+                .spec(getRequestBuilder(host + path).build())
                 .when()
-                .get(host + path + "?path=/")
+                .get(host + path + "?path=" + pathToFileOrFolder)
                 .then()
-                .assertThat()
-                .body(matchesJsonSchemaInClasspath("getFileOrFolderMetaInfo.json"))
-                .and()
-                .body("_embedded.total", equalTo(10));
+                .extract()
+                .response();
     }
+
+    @Step("PUT Запрос: Создание новой папки")
+    public Response putCreateNewFolder(String folderName){
+        return givenWithAuth()
+                .spec(getRequestBuilder(host + path).build())
+                .when()
+                .put(host + path + "?path=" + folderName)
+                .then()
+                .statusCode(201)
+                .extract()
+                .response();
+    }
+
+    @Step("DELETE Запрос: Удаление файла или папки")
+    public void deleteFileOrFolder(String folderName){
+        givenWithAuth()
+                .spec(getRequestBuilder(host + path).build())
+                .when()
+                .delete(host + path + "?path=" + folderName)
+                .then()
+                .statusCode(204);
+    }
+
 }
--- src/test/java/tests/api/TestCRUD.java
+++ src/test/java/tests/api/TestCRUD.java
@@ -3,10 +3,17 @@ package tests.api;
 import api.requests.DiskManagementRequest;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
+import io.restassured.response.Response;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class TestCRUD {
     DiskManagementRequest request;
+    String folderName = RandomStringUtils.randomAlphanumeric(12);
+    String responsePath = "https://cloud-api.yandex.net/v1/disk/resources?path=disk%3A%2F";
+    Response response;
 
     public TestCRUD(){
         request = new DiskManagementRequest();
@@ -15,12 +22,36 @@ public class TestCRUD {
     @Severity(SeverityLevel.NORMAL)
     @Test(description = "Получить метаинформацию о диске пользователя")
     public void testGetMetaInfoAboutUsersDisk() {
-        request.getDiskMetaInfo();
+        response = request.getDiskMetaInfo();
+        response.then()
+                .assertThat()
+                .body(matchesJsonSchemaInClasspath("getMetaInfo.json"));
     }
 
     @Severity(SeverityLevel.NORMAL)
-    @Test(description = "Получить метаинформацию о файле или каталоге")
+    @Test(description = "Получить метаинформацию корневом каталоге", dependsOnMethods = "testDeleteFolder")
     public void testGetFileOrFolderMetaInfo() {
-        request.getFileOrFolderMetaInfo();
+        response = request.getFileOrFolderMetaInfo("/");
+        response.then()
+                .assertThat()
+                .body(matchesJsonSchemaInClasspath("getFileOrFolderMetaInfo.json"));
     }
+
+    @Severity(SeverityLevel.CRITICAL)
+    @Test(description = "Создать новую папку {folderName}")
+    public void testCreateNewFolder() {
+        response = request.putCreateNewFolder(folderName);
+
+        Assert.assertEquals(response.path("href"), responsePath + folderName);
+    }
+
+    @Severity(SeverityLevel.CRITICAL)
+    @Test(description = "Удалить папку {folderName}", dependsOnMethods = "testCreateNewFolder")
+    public void testDeleteFolder() {
+        request.deleteFileOrFolder(folderName);
+        response = request.getFileOrFolderMetaInfo("/" + folderName);
+
+        Assert.assertEquals(response.path("description"), "Resource not found.");
+    }
+
 }
